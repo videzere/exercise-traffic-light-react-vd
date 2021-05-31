aa30d63a899c4c7ebf97b86bbb0657f692b53214
Victoria Dezerega <videzere@gmail.com>
2021-05-30 21:43:11 -0400

    Actualizar GitHud v1 from VS

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,65 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("bg-danger");
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Container className="location pt-0">
+			<Row>
+				<Col
+					xs={{ span: 7, offset: 5 }}
+					className="bg-dark w-auto h-auto p-2"></Col>
+			</Row>
+			<Row>
+				<Col
+					xs={{ span: 7, offset: 5 }}
+					className="bg-dark w-auto h-auto p-2"></Col>
+			</Row>
+			<Row>
+				<Col
+					xs={{ span: 7, offset: 5 }}
+					className="bg-dark w-auto h-auto p-2"></Col>
+			</Row>
+			<Row>
+				<Col xs={5}></Col>
+				<Col xs={2} className="location bg-dark square rounded-4">
+					<Row>
+						<Col xs={3}></Col>
+						<Col
+							xs={1}
+							onClick={() => setColor("bg-danger")}
+							className={
+								"light bg-danger " +
+								(color === "bg-danger" ? "glow" : "")
+							}></Col>
+						<Col xs={3}></Col>
+					</Row>
+					<Row>
+						<Col xs={3}></Col>
+						<Col
+							xs={1}
+							onClick={() => setColor("bg-warning")}
+							className={
+								"light bg-warning " +
+								(color === "bg-warning" ? "glow" : "")
+							}></Col>
+						<Col xs={3}></Col>
+					</Row>
+					<Row>
+						<Col xs={3}></Col>
+						<Col
+							xs={1}
+							onClick={() => setColor("bg-success")}
+							className={
+								"light bg-success " +
+								(color === "bg-success" ? "glow" : "")
+							}></Col>
+						<Col xs={3}></Col>
+					</Row>
+				</Col>
+				<Col xs={5}></Col>
+			</Row>
+		</Container>
 	);
 }
